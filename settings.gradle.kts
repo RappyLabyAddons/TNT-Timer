4bfc3f567bafc1c20bcf5e20b8bd963ad8c5f98f
RappyTV <rappytv@rappytv.com>
2025-11-02 18:45:19 +0100

    chore: Update build scripts and dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,8 @@
-rootProject.name = "tnttimer-addon-labymod"
+rootProject.name = "tnttimer"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.3.22"
+    val labyGradlePluginVersion = "0.5.9"
+
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
@@ -9,17 +10,19 @@ pluginManagement {
     buildscript {
         repositories {
             maven("https://dist.labymod.net/api/v1/maven/release/")
-            maven("https://repo.spongepowered.org/repository/maven-public")
+            maven("https://maven.neoforged.net/releases/")
+            maven("https://maven.fabricmc.net/")
+            gradlePluginPortal()
             mavenCentral()
         }
 
         dependencies {
-            classpath("net.labymod.gradle", "addon", labyGradlePluginVersion)
+            classpath("net.labymod.gradle", "common", labyGradlePluginVersion)
         }
     }
 }
 
-plugins.apply("net.labymod.gradle")
+plugins.apply("net.labymod.labygradle.settings")
 
 include(":api")
 include(":core")
